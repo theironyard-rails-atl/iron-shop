6d2cba9dbcab32e522db923fd8583e1b4559d454
Matthew Thompson <matt@mattathompson.org>
2014-09-22 14:14:44 -0400

    creating item & invoice models

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140922180455) do
+ActiveRecord::Schema.define(version: 20140922181021) do
+
+  create_table "invoices", force: true do |t|
+    t.float    "amount"
+    t.integer  "shopper_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "items", force: true do |t|
+    t.string   "title"
+    t.text     "description"
+    t.float    "price"
+    t.integer  "seller_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "",    null: false
