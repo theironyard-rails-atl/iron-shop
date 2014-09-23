f4432b5be83cdcae7abb3c03edf9ccc463e99265
James Dabbs <jamesdabbs@gmail.com>
2014-09-23 11:20:16 -0400

    Refactor service object

--- app/services/card_processor.rb
+++ app/services/card_processor.rb
@@ -4,21 +4,35 @@ class CardProcessor
   end
 
   def process
+    @customer = create_customer
+    process_charge! @customer
+    note_payment
+    # print receipt?
+    # send email?
+  end
+
+private
+
+  def create_customer
     # Makes a POST call to create a Customer resource
     #   on the Stripe API
-    customer = Stripe::Customer.create(
+    Stripe::Customer.create(
       :email => @invoice.user.email,
       :card  => @token
     )
+  end
 
+  def process_charge! customer
     # Make a POST call to actually charge user's card
-    charge = Stripe::Charge.create(
+    Stripe::Charge.create(
       :customer    => customer.id,
       :amount      => @invoice.amount_in_cents,
       :description => 'Iron Shop customer',
       :currency    => 'usd'
     )
+  end
 
+  def note_payment
     @invoice.paid = true
     @invoice.save!
   end
