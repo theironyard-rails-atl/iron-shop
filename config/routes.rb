5aa5ef17f81712b0ef5e9f4b1ec8f53ea270a139
Andrew House <andrewhouse621@gmail.com>
2014-09-23 10:01:44 -0400

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
 
   root to: "static_pages#home"
 
-  resources :invoices, only: [:show] do 
-    member do 
-     post :close
+  resource :invoices, only: [:show] do
+    member do
+      post :close
     end
   end
 end
