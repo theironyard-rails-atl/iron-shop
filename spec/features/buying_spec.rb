883d4b0ca74f17782b67dfd8b114cfc8df1a4764
Riaz Virani <riaz.n.virani@gmail.com>
2014-09-24 18:48:04 -0400

    Finished cart specs

--- spec/features/buying_spec.rb
+++ spec/features/buying_spec.rb
@@ -5,17 +5,32 @@ feature 'Buying', :js, :focus do
     @user = create :user
     login @user
 
-    4.times do
-      create :item
-    end
+    create :item
   end
 
   it 'lets buyers search for items'
   it 'lets buyers add items to their cart' do
+    visit cart_path
+    expect(page).not_to have_css('.item')
     visit items_path
     click_link("Add to Cart", match: :first)
+    item = Item.first
+    expect(page).to have_css('.item')
   end
-  it 'lets buyers view their cart'
-  it 'lets buyers remove items from their cart'
+  it 'lets buyers view their cart' do
+    visit items_path
+    click_link("Add to Cart", match: :first)
+    item = Item.first
+    expect(page).to have_content(item.title)
+    expect(page).to have_content(item.description)
+    expect(page).to have_content(item.price)
+  end
+  it 'lets buyers remove items from their cart' do
+    visit items_path
+    click_link("Add to Cart", match: :first)
+    expect(page).to have_css('.item')
+    find('.remove-button').click
+    expect(page).not_to have_css('.item')
+  end
   it 'lets buyers checkout'
 end
