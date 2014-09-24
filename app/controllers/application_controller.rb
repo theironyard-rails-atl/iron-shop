33b7e072fb05a8bdcd3c2420c7b2e46de6a0ce8c
Taylor Martin <taylormartin.uga@gmail.com>
2014-09-24 14:14:03 -0400

    basic item CRUD

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,12 +4,29 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+
   rescue_from CanCan::AccessDenied do |exception|
     if current_user
       redirect_to root_path, :alert => exception.message
     else
       redirect_to new_user_session_path, :alert => "Please sign in"
     end
+  end
+
+protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) do |u|
+      u.permit(:role, :email, :password, :password_confirmation)
+    end
+  end
+
+private
+
+  def render_invalid obj
+    render json: { errors: obj.errors.full_messages }, status: 422
   end
 
 end
