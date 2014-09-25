7cbb17e1a320110d9f3437f0e2ed6d59c2e57421
Taylor Martin <taylormartin.uga@gmail.com>
2014-09-25 11:02:49 -0400

    listing feature spec

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,6 +7,7 @@ class ItemsController < ApplicationController
   def create
     @item = Item.new create_params.merge(seller_id: current_user.id)
     if @item.save
+      flash[:notice] = "Listing successfully created"
       render :show
     else
       render_invalid @item
@@ -24,6 +25,7 @@ class ItemsController < ApplicationController
   def update
     @item = Item.find params[:id]
     if @item.update update_params
+      flash[:notice] = "Listing successfully updated"
       render :show
     else
       render_invalid @item
