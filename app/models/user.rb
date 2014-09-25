8da87bab4eecf2f935e5c4d332ce43f82169a45e
Khin Latt <k.maunglatt@gmail.com>
2014-09-25 13:51:42 -0400

    Set up Devise background mailer

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,7 +23,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :async
 
   has_many :items, foreign_key: 'seller_id'
   has_many :invoices, foreign_key: 'shopper_id'
