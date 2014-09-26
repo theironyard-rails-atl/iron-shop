79e3db6e1c36b963ba485f990cfbe9dbf696e5f3
Robert Groller <robertgroller@gmail.com>
2014-09-26 15:02:59 -0400

    Pull pg_search and update schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140925171655) do
+ActiveRecord::Schema.define(version: 20140925234028) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
